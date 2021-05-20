57d63a13d38b344a178d7bbe9bcfb433f6e75fd1
Marco Antonio Granados Vargas <marcovafe@gmail.com>
2021-05-19 18:26:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Instalacion y Uso
+
+Este proyecto fue realizado con la ayuda de la extención de Visual Studio Code llamada [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) para hacer uso de este proyecto se necesita usar dicha extención posicionandose en el archivo [index.html](/index.html) y hacer lo siguiente:
+![liveserverexample](/img/live_server.jpg)
+Usar la opcion que dice Abrir con Live Server.
---
+++ src/main.js
@@ -0,0 +1,83 @@
+//Inicializamos la carga de todos los items con la llamada al funcion 'loadData()'
+window.onload = () => {
+  loadData();
+};
+
+/*  Esta funcion asincrona recoleta la informacion del 'data.json'
+    una vez cargado el json pasaremos a la creacion de los items utilizando 
+    la funcion llamada 'createItem()'
+*/
+async function loadData() {
+  var dataJson = [];
+  await fetch("../data/data.json")
+    .then((data) => data.json())
+    .then((json) => {
+      dataJson = json;
+    });
+
+  dataJson.forEach((element) => createItem(element));
+}
+
+/**
+ * Esta funcion tiene como objetivo crear los diferentes
+ * items que estan contenidos en el data.json
+ * aqui se toma como parametro un solo item que contiene
+ * el precio, nombre, imagen e id del mismo
+ * @param {json} element
+ * @returns void
+ */
+function createItem(element) {
+  //Definimos el componente padre recolectandolo del html principal.
+  var parentGrid = document.getElementById("grid-container");
+  //Se definen las variables a usar para poder observar los items.
+  var itemDivParent = document.createElement("div");
+  var itemDivImg = document.createElement("div");
+
+  var itemDescrpParent = document.createElement("div");
+  var itemPrice = document.createElement("div");
+  var itemName = document.createElement("div");
+
+  //Descripcion de Item Componente Padre, se añade la clase respectiva
+  itemDescrpParent.classList.add("itemDescrp");
+  /*  Añadir clases definidas para el precio y verificacion de que el elemento contiene
+    el item 'price', en caso de no tenerlo se prueba con 'newprice' y se añade la etiqueta 'Nuevo'
+*/
+  if (element.price) {
+    //Se añade el contenido y las clases respectivas para mostrar el precio del item
+    itemPrice.classList.add("itemPrice");
+    itemPrice.classList.add("left");
+    itemPrice.innerHTML = "₡" + element.price;
+  } else {
+    //Se añade el contenido y las clases respectivas para mostrar el precio del item
+    itemPrice.classList.add("itemPrice");
+    itemPrice.classList.add("left");
+    itemPrice.innerHTML = "₡" + element.new_price;
+    //Se añade la variable innerNewSpan por ser un elemento con caracteristica 'newprice'
+    var itemNewSpan = document.createElement("div");
+    itemNewSpan.innerHTML = "Nuevo";
+    itemNewSpan.classList.add("imgNewItem");
+    itemNewSpan.classList.add("bottom");
+    itemNewSpan.classList.add("right");
+    itemDivImg.appendChild(itemNewSpan);
+  }
+  //Se añade el nombre del item
+  itemName.classList.add("itemName");
+  itemName.classList.add("left");
+  itemName.innerHTML = element.name;
+
+  //se agrega la clase respectiva para el container padre y el id del item
+  itemDivParent.classList.add("grid-item");
+  itemDivParent.id = element.id;
+  itemDivImg.classList.add("imgItem");
+  //Se añade la imagen del producto al contenedor respectivo
+  itemDivImg.style.backgroundImage = `url(${element.image})`;
+  /* Por ultimo se añaden todos las variables al contenedor principal del item y tambien se
+    agrega al padre de todos los items.
+*/
+  itemDescrpParent.appendChild(itemPrice);
+  itemDescrpParent.appendChild(itemName);
+  itemDivParent.appendChild(itemDivImg);
+  itemDivParent.appendChild(itemDescrpParent);
+  parentGrid.appendChild(itemDivParent);
+  return;
+}
